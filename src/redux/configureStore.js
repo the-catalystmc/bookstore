b94494f81110164596a86d4db617d0dc1fa4ae16
Rhyine <rhyinestewart@gmail.com>
2021-08-24 10:00:51 -0500

    configure redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import booksReducer from './books/books';
 
 const reducers = combineReducers({
   books: booksReducer,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 
 export default store;
