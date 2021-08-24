cf906d655ea620581264c5d57fb69196778c1cb3
Rhyine <rhyinestewart@gmail.com>
2021-08-24 00:22:02 -0500

    Setup books redux info

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,46 @@
 // CNSTANTS
-const ADD_BOOK = 'ADD_BOOK'
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
 
 // ACTIONS
-const addBook =() => {
-    return {
-        type: ADD_BOOK,
-    }
+export const addBook = () => {
+  return {
+    type: ADD_BOOK,
+    payload,
+  }
 }
+
+export const removeBook = () => {
+  return {
+    type: REMOVE_BOOK,
+    payload,
+  }
+}
+
+// REDUCERS
+export const initialState = [
+  {
+    id: '1',
+    title: 'Lord of The Flies',
+    author: 'William Golding',
+    genre: 'Drama',
+  }
+]
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        action.payload,
+      ]
+
+      case REMOVE_BOOK:
+        return state.filter((item) => booksReducer.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+}
+
+export default booksReducer;
