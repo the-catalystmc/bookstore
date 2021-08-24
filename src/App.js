cdb80802f3525074a882b189325511d6713cbfaf
Rhyine <rhyinestewart@gmail.com>
2021-08-24 09:05:52 -0500

    Update my code to be cleaner

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route, BrowserRouter as Router, Switch,
+} from 'react-router-dom';
+import store from './redux/configureStore';
+import NavBar from './components/NavBar';
+import Categories from './components/Categories';
+import Books from './components/Books';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <div className="App">
+            <NavBar />
+            <Route path="/categories"><Categories text="Under Construction." /></Route>
+            <Route path="/" exact><Books /></Route>
+          </div>
+        </Switch>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import {
-  Route, BrowserRouter as Router, Switch, Link,
-} from 'react-router-dom';
-import store from './redux/configureStore';
-import Categories from './components/Categories';
-import Books from './components/Books';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <div className="Nav-Container">
-          <h1>Bookstore CMS</h1>
-          <nav>
-            <ul className="Nav-Items">
-              <li className="Nav-Item"><Link exact to="/">Books</Link></li>
-              <li className="Nav-Item"><Link to="/categories">Categories</Link></li>
-            </ul>
-          </nav>
-        </div>
-        <Switch>
-          <div className="App">
-            <Route path="/categories"><Categories text="Under Construction." /></Route>
-            <Route path="/" exact><Books /></Route>
-          </div>
-        </Switch>
-      </Router>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
