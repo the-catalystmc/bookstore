1a2993a424116ccfa1f05c4f01864ad40ebeab36
Rhyine <rhyinestewart@gmail.com>
2021-08-25 23:25:38 -0500

    update add book event

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,49 +1,42 @@
 import { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { v4 as uuidv4 } from 'uuid';
-// import { addBook } from '../redux/books/books';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const Form = () => {
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [genre, setGenre] = useState('Drama');
+  const [category, setCategory] = useState('Drama');
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const titleChanged = (e) => setTitle(e.target.value);
-  const authorChanged = (e) => setAuthor(e.target.value);
-  const genreChanged = (e) => setGenre(e.target.value);
+  const categoryChanged = (e) => setCategory(e.target.value);
 
-  // const submitBookToStore = (e) => {
-  //   e.preventDefault();
-  //   if (title && genre && author) {
-  //     const newBook = {
-  //       id: uuidv4(),
-  //       title,
-  //       author,
-  //       genre,
-  //     };
-
-  //     dispatch an action and pass it the newBook object (your action's payload)
-  //     dispatch(addBook(newBook));
-
-  //     setTitle('');
-  //     setAuthor('');
-  //     setGenre('Drama');
-  //   }
-  // };
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    if (title && category) {
+      dispatch(
+        addBook({
+          item_id: uuidv4(),
+          category,
+          title,
+        }),
+      );
+      setTitle('');
+      setCategory('Drama');
+    }
+  };
 
   return (
     <div>
       <form>
         <h2>ADD NEW BOOK</h2>
         <input type="text" name="title" value={title} onChange={titleChanged} placeholder="Title" required />
-        <input type="text" name="author" value={author} onChange={authorChanged} placeholder="Author" required />
-        <select name="genre" value={genre} onChange={genreChanged}>
+        <select name="category" value={category} onChange={categoryChanged}>
           <option value="Drama">Drama</option>
           <option value="Action">Action</option>
         </select>
-        <input type="submit" value="ADD BOOK" />
+        <input type="submit" value="ADD BOOK" onClick={submitBookToStore} />
       </form>
     </div>
   );
