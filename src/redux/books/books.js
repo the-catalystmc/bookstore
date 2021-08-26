17bb7a18e90afd4eb5cdce2318752fa1ef416281
Rhyine <rhyinestewart@gmail.com>
2021-08-26 01:24:20 -0500

    remove comments from code

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,12 @@
 /* eslint-disable camelcase */
-// import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Cr80pbkuf1RrEQZCSqfj/books
-// ACTION TYPES
 const REMOVE_BOOK_SUCCESS = 'REMOVE_BOOK_SUCCESS';
 const ADD_BOOK_SUCCESS = 'ADD_BOOK_SUCCESS';
 const GET_BOOK_REQUEST = 'GET_BOOK_REQUEST';
 const GET_BOOK_SUCCESS = 'GET_BOOK_SUCCESS';
 const GET_BOOK_FAILURE = 'GET_BOOK_FAILURE';
 
-// ACTION CREATORS
 export const getBookRequest = () => ({
   type: GET_BOOK_REQUEST,
 });
@@ -84,7 +80,6 @@ export const removeBook = (id) => (dispatch) => {
     });
 };
 
-// REDUCERS
 export const initialState = {
   loading: false,
   books: [],
@@ -126,39 +121,3 @@ const booksReducer = (state = initialState, action) => {
 };
 
 export default booksReducer;
-
-// // CONSTANTS
-// const ADD_BOOK = 'ADD_BOOK';
-// const REMOVE_BOOK = 'REMOVE_BOOK';
-
-// // ACTIONS
-// export const addBook = (payload) => (
-//   {
-//     type: ADD_BOOK,
-//     payload,
-//   });
-
-// export const removeBook = (payload) => (
-//   {
-//     type: REMOVE_BOOK,
-//     payload,
-//   });
-
-// // REDUCERS
-// export const initialState = [];
-
-// const booksReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ADD_BOOK:
-//       return [
-//         ...state,
-//         action.payload,
-//       ];
-
-//     case REMOVE_BOOK:
-//       return state.filter((item) => item.id !== action.payload.id);
-
-//     default:
-//       return state;
-//   }
-// };
