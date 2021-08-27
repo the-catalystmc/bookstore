f660dc8922c466921a1bb7417d28fff9c9ba096f
Rhyine <rhyinestewart@gmail.com>
2021-08-27 09:40:14 -0500

    Move Provider to index

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import {
   Route, BrowserRouter as Router, Switch,
 } from 'react-router-dom';
-import store from './redux/configureStore';
 import NavBar from './components/NavBar';
 import Categories from './components/Categories';
 import Books from './components/Books';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <div className="App">
-            <NavBar />
-            <Route path="/categories"><Categories text="Under Construction." /></Route>
-            <Route path="/" exact><Books /></Route>
-          </div>
-        </Switch>
-      </Router>
-    </Provider>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <div className="App">
+          <NavBar />
+          <Route path="/categories"><Categories text="Under Construction." /></Route>
+          <Route path="/" exact><Books /></Route>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
